0a9bca2ef90491972bf2eb2c9d86a5715410a45a
benrobertsonio <ben@robertson.is>
2023-03-27 15:16:38 -0400

    How about this?

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,12 @@
 import "./globals.css";
 
+const today = new Date();
+
+export const metadata = {
+	title: "Percent Complete",
+	description: `Progress bar showing how far through the year we are in ${today.getFullYear()}.`,
+};
+
 export default function RootLayout({
 	children,
 }: {
