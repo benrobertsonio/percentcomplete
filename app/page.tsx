a71007f8b894dfc270cd3e4c8e879966272ea966
benrobertsonio <ben@robertson.is>
2023-03-27 15:09:53 -0400

    Add head.

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { Press_Start_2P } from "next/font/google";
 import "nes.css/css/nes.min.css";
+import Head from "next/head";
 
 const pressStart2P = Press_Start_2P({ weight: "400", subsets: ["latin"] });
 
@@ -23,64 +24,79 @@ export default function Home() {
 	const percentNumber = (timeSince / msInYear) * 100;
 
 	return (
-		<div className={pressStart2P.className}>
-			<div
-				style={{
-					height: "100vh",
-					width: "100vw",
-					display: "flex",
-					justifyContent: "center",
-					alignItems: "center",
-					flexDirection: "column",
-					backgroundColor: "#212529",
-				}}
-			>
-				<main
+		<>
+			<Head>
+				<title>Percent Complete</title>
+				<meta property="og:title" content="Percent Complete" />
+				<meta property="og:type" content="webpage" />
+				<meta property="og:url" content="https://percentcomplete.netlify.app" />
+				<meta property="og:site_name" content="Percent Complete" />
+				<meta
+					name="description"
+					content={`Progress bar showing how far through the year we are in ${today.getFullYear()}.`}
+				/>
+			</Head>
+			<div className={pressStart2P.className}>
+				<div
 					style={{
-						width: "90%",
-						margin: "0 auto",
-						maxWidth: "70ch",
-						textAlign: "center",
+						height: "100vh",
+						width: "100vw",
+						display: "flex",
+						justifyContent: "center",
+						alignItems: "center",
+						flexDirection: "column",
+						backgroundColor: "#212529",
 					}}
 				>
-					<section className="nes-container with-title is-centered is-dark">
-						<h1 className="nes-text title">
-							Today is{" "}
-							{today.toLocaleDateString(undefined, {
-								weekday: "long",
-								year: "numeric",
-								month: "long",
-								day: "numeric",
-							})}
-							.
-						</h1>
-						<h1>
-							<span className="nes-text">
-								The year {new Date().getFullYear()} is{" "}
-								<span className="nes-text is-success">{percent}</span> complete.
-							</span>
-						</h1>
-						<br />
-						<progress
-							className="nes-progress is-pattern"
-							value={percentNumber}
-							max={100}
-						/>
-						<br />
-						<p
-							style={{ marginTop: "1em", fontSize: "1.2em" }}
-							className="nes-text"
-						>
-							{percent}
+					<main
+						style={{
+							width: "90%",
+							margin: "0 auto",
+							maxWidth: "70ch",
+							textAlign: "center",
+						}}
+					>
+						<section className="nes-container with-title is-centered is-dark">
+							<h1 className="nes-text title">
+								Today is{" "}
+								{today.toLocaleDateString(undefined, {
+									weekday: "long",
+									year: "numeric",
+									month: "long",
+									day: "numeric",
+								})}
+								.
+							</h1>
+							<h1>
+								<span className="nes-text">
+									The year {new Date().getFullYear()} is{" "}
+									<span className="nes-text is-success">{percent}</span>{" "}
+									complete.
+								</span>
+							</h1>
+							<br />
+							<progress
+								className="nes-progress is-pattern"
+								value={percentNumber}
+								max={100}
+							/>
+							<br />
+							<p
+								style={{ marginTop: "1em", fontSize: "1.2em" }}
+								className="nes-text"
+							>
+								{percent}
+							</p>
+						</section>
+					</main>
+					<footer style={{ marginTop: "2em" }}>
+						<p className="nes-text" style={{ color: "white" }}>
+							Made with <i className="nes-icon heart is-small" /> by Ben
+							Robertson
 						</p>
-					</section>
-				</main>
-				<footer style={{ marginTop: "2em" }}>
-					<p className="nes-text" style={{ color: "white" }}>
-						Made with <i className="nes-icon heart is-small" /> by Ben Robertson
-					</p>
-				</footer>
+					</footer>
+				</div>
 			</div>
-		</div>
+		</>
 	);
 }
