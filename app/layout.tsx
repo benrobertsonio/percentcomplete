7b8a613a82219f805991548a2c65b82ce5fda1d6
Ben Robertson <ben@robertson.is>
2024-03-11 12:13:26 -0400

    Add analytics.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,11 @@
+import Script from "next/script";
 import "./globals.css";
 
 const today = new Date();
 
 export const metadata = {
-	title: "Percent Complete",
-	description: `Progress bar showing how far through the year we are in ${today.getFullYear()}.`,
+	title: "Calculating.World",
+	description: `Free online calculators for date math, time calculations, percentages of years/months/days, and tracking time periods. Find elapsed and remaining times easily.`,
 };
 
 export default function RootLayout({
@@ -14,6 +15,9 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="en">
+			<Script
+				src="https://beamanalytics.b-cdn.net/beam.min.js"
+				data-token="9ea5cf2c-e873-4c7c-95b3-763d2a6440d4" async />
 			<body>{children}</body>
 		</html>
 	);
